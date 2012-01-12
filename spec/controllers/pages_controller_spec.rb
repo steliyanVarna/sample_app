bf928a2d1e2a0e736678c6ed00d4e1a681070b89
Steliyan Staklev <stelyians@facebook.com>
2012-01-12 01:21:33 +0100

    added layout

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,24 +1,35 @@
 require 'spec_helper'
 
 describe PagesController do
+render_views
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
-    end
-  end
-
+      end
+      it "should not have blank body" do
+	get 'home'
+	response.body.should_not =~ /<body>\s*<\/body>/
+      end
+    
+   end
+  
+  
+   
   describe "GET 'contact'" do
     it "returns http success" do
       get 'contact'
       response.should be_success
     end
+    
   end
-  describe "GET 'about method' " do
+  
+   describe "GET 'about' " do
     it "returns about method created" do
       get 'about'
       response.should be_success
     end
+   
   end
 end
